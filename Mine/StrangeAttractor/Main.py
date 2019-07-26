3bcb6d86b8814260dd55298215a336700e830943
tyst3273 <ty.sterling@colorado.edu>
2019-07-26 16:19:07 -0600

    autopush

--- Mine/StrangeAttractor/Main.py
+++ Mine/StrangeAttractor/Main.py
@@ -3,26 +3,18 @@ import matplotlib.pyplot as plt
 import Duffing
 
 ### time step, number of steps
-dt = 0.0005
-steps = int(100e3)
+dt = 0.0026179938779914945
+steps = int(100e6)
 time = np.arange(0,steps)*dt
+
 
 ### initial conditions
 particle_1 = Duffing.particle(
         position=0.5,
         velocity=0.0,
         mass=1.0,
-        a=0.25,
-        b=0.5,
-        f0=2.0,
-        omega=2.4,
-        gamma=0.1)
-particle_2 = Duffing.particle(
-        position=0.5001,
-        velocity=0.0,
-        mass=1.0,
-        a=0.25,
-        b=0.5,
+        a=-1.0,
+        b=1.0,
         f0=2.0,
         omega=2.4,
         gamma=0.1)
@@ -32,51 +24,43 @@ velocity_1 = np.zeros(steps)
 position_1 = np.zeros(steps)
 force_1 = np.zeros(steps)
 
-velocity_2 = np.zeros(steps)
-position_2 = np.zeros(steps)
-force_2 = np.zeros(steps)
-
 velocity_1[0] = particle_1.velocity
 position_1[0] = particle_1.position
 force_1[0] = particle_1.force
 
-velocity_2[0] = particle_2.velocity
-position_2[0] = particle_2.position
-force_2[0] = particle_2.force
-
 ### integrate
 for i in range(1,steps):
+    if i%10000 == 0 and i != 0:
+        print('Step {} out of {}'.format(i,steps))
     particle_1.runge_kutta(dt)
-    particle_2.runge_kutta(dt)
 
     velocity_1[i] = particle_1.velocity
     position_1[i] = particle_1.position
     force_1[i] = particle_1.force
 
-    velocity_2[i] = particle_2.velocity
-    position_2[i] = particle_2.position
-    force_2[i] = particle_2.force
+#pe_1 = (-particle_1.forceparams['gamma']*velocity_1
+#        +2*particle_1.forceparams['a']*position_1
+#        -4*particle_1.forceparams['b']*position_1**3
+#        +particle_1.forceparams['f0']*np.cos(particle_1.forceparams['omega']*time))
 
 ### save the data
+period = 1000
 data = np.append(time.reshape(steps,1),position_1.reshape(steps,1),axis=1)
 data = np.append(data,velocity_1.reshape(steps,1),axis=1)
-data = np.append(data,position_2.reshape(steps,1),axis=1)
-data = np.append(data,velocity_2.reshape(steps,1),axis=1)
-np.savetxt('duffing.csv',data)
+np.savetxt('duffing.csv',data[::period,:])
 
 ### plot
 fig, ax = plt.subplots()
-fig.set_size_inches(5,4,forward=True)
+fig.set_size_inches(5,3,forward=True)
 fig.tight_layout(pad=2.5)
 
-ax.plot(time,position_1,'r',lw=1.5,label=r'$x_0$=0.5')
-ax.plot(time,position_2,'b',lw=1.5,label=r'$x_0$=0.5001')
+ax.plot(time,position_1,'k',lw=1.5) #,label=r'$x_0$=0.5')
 
 # configure the plot
 for axis in ['top','bottom','left','right']:
   ax.spines[axis].set_linewidth(1.5)
 
-ax.set_ylim(-3,3)
+#ax.set_ylim(-3,3)
 #ax.set_xlim(-0.02,0.52)
 
 ax.minorticks_on()
@@ -87,8 +71,8 @@ ax.set_xlabel('time',labelpad=4,fontweight='normal',
               fontsize='large')
 ax.set_ylabel('position',labelpad=4,fontweight='normal',
               fontsize='large')
-ax.legend(frameon=False,prop={'size':'small'},ncol=2)
+#ax.legend(frameon=False,prop={'size':'small'},ncol=2)
 fig.suptitle('Duffing Oscillator',y=0.97)
-plt.savefig('duffing.png',format='png',dpi=300,bbox_inches='tight')
+#plt.savefig('duffing.png',format='png',dpi=300,bbox_inches='tight')
 plt.show()
 
--- Mine/StrangeAttractor/PlotPoincare.py
+++ Mine/StrangeAttractor/PlotPoincare.py
@@ -4,15 +4,15 @@ import Duffing
 
 data = np.loadtxt('duffing.csv')
 
+skip = int(10e3)
+
 ### plot
 fig, ax = plt.subplots()
-fig.set_size_inches(6,4,forward=True)
+fig.set_size_inches(4,4,forward=True)
 fig.tight_layout(pad=2.5)
 
-#ax.plot(data[:,0],data[:,1],'r',lw=1.5,label=r'$x_0$=0.5')
-#ax.plot(data[:,0],data[:,3],'b',lw=1.5,label=r'$x_0$=0.5001')
-
-ax.scatter(data[:,1],data[:,2],s=1)
+#ax.plot(data[:,0],data[:,1],'r',lw=1.5) #,label=r'$x_0$=0.5')
+ax.scatter(data[50:,1],data[50:,2],s=0.05)
 
 # configure the plot
 for axis in ['top','bottom','left','right']:
@@ -29,8 +29,8 @@ ax.set_xlabel('Position',labelpad=4,fontweight='normal',
               fontsize='large')
 ax.set_ylabel('Momentum',labelpad=4,fontweight='normal',
               fontsize='large')
-ax.legend(frameon=False,prop={'size':'small'},ncol=2)
-fig.suptitle('Strange Attractor',y=0.97)
-#plt.savefig('strange_attractor.png',format='png',dpi=300,bbox_inches='tight')
+#ax.legend(frameon=False,prop={'size':'small'},ncol=2)
+fig.suptitle('Strange Attractor',y=0.95)
+plt.savefig('strange_attractor.png',format='png',dpi=300,bbox_inches='tight')
 plt.show()
 
